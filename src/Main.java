f2f9ee5996489ed40531c3ba2f72c5cd80d92661
Max Livingston <liv.max02@gmail.com>
2024-07-05 20:46:12 +0600

    Done a simple test case

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Path path1 = Paths.get("src/test1.txt");
+        Path path2 = Paths.get("src/test2.txt");
+
+        String fileStr1 = Files.readString(path1);
+        String fileStr2 = Files.readString(path2);
+
+        System.out.println(fileStr1 + fileStr2);
     }
 }
