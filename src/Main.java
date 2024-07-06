5861150fd9b5b5aba32502c16341dbeb21596d7e
Max Livingston <liv.max02@gmail.com>
2024-07-06 13:24:54 +0600

    Added a bit of configuration

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,28 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     @SuppressWarnings("FieldCanBeLocal")
     private final String REGEX_PATTERN = "require '([^']+)'";
+
+    private final List<String> files = Arrays.asList(
+            "src/test1.txt",
+            "src/test2.txt",
+            "src/test3.txt"
+    );
+
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         main.run();
     }
 
     private void run() throws IOException {
-        String filePath = "src/test1.txt";
+        String filePath = files.get(0);
         String txtFile1 = Files.readString(Paths.get(filePath));
 
         Matcher matcher = Pattern.compile(REGEX_PATTERN).matcher(txtFile1);
